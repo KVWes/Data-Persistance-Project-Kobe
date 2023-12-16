297352cee5c2280b5c52912f92c43c2fa98d94a4
KVWes <kvbusiness20@gmail.com>
2023-12-16 16:34:17 -0500

    final commit
    Added multiple scripts and code for high score functionality.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,23 +11,38 @@ public class MainManager : MonoBehaviour
     public int LineCount = 6;
     public Rigidbody Ball;
 
-    public Text ScoreText;
+    public static MainManager Instance;
+
+    [SerializeField] public TMP_Text ScoreText;
+    [SerializeField] public TMP_Text bestScoreText;
     public GameObject GameOverText;
-   
-    
+
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        MenuManager.Instance.LoadGameInfo();
+        bestScoreText.text = "Best Score: " + MenuManager.Instance.bestName + " : " + MenuManager.Instance.bestScore;
+    }
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -72,7 +87,10 @@ public class MainManager : MonoBehaviour
 
     public void GameOver()
     {
+        MenuManager.Instance.SetBestScore(m_Points);
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
+
+
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,34 +4,113 @@ using System.Web.UI.WebControls;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using System.IO;
+using JetBrains.Annotations;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public static MenuManager instance;
+    public static MenuManager Instance;
 
-    public TMP_InputField userNameText;
+    [SerializeField] TMP_InputField nameField;
+    [SerializeField] TMP_Text bestScoreText;
 
+    public string userName = "";
+    public string bestName;
+    public int bestScore;
+
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadGameInfo();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        userNameText = GetComponent<TMP_InputField>();
+       if (bestName != "")
+        {
+            bestScoreText.text = "Best Score: " + bestName + " : " + bestScore;
+        }
     }
 
-    public void StarNew()
+    public void SetBestScore(int score)
     {
-        SceneManager.LoadScene(1);
+        if (score > bestScore)
+        {
+            bestScore = score;
+            bestName = userName;
+            SaveGameInfo();
+            MainManager.Instance.bestScoreText.text = "Best Score : " + bestName + " : " + bestScore;
+        }
+
+        Debug.Log("Score: " + score + " Player: " + userName);
+    }
+
+    public void StartNew()
+    {
+        if (nameField.text != "")
+        {
+            userName = nameField.text;
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.LogWarning("Please enter a name!");
+        }
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 
     // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    class SaveData
     {
-        
+        public string userName;
+        public int bestScore;
     }
 
-    private void Awake()
+    public void SaveGameInfo()
     {
-       instance = this;
-       DontDestroyOnLoad(gameObject);
+        SaveData data = new SaveData();
+        data.userName = bestName;
+        data.bestScore = bestScore;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadGameInfo()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            bestScore = data.bestScore;
+            bestName = data.userName;
+        }
     }
 }
